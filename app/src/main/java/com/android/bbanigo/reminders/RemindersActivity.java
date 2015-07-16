750672b36d3f80d79a708ad1483d03011ef73747
Boma Banigo <bbanigo@hotmail.com>
2015-07-15 23:27:18 -0500

    Adds new reminder and exit menu options

--- app/src/main/java/com/android/bbanigo/reminders/RemindersActivity.java
+++ app/src/main/java/com/android/bbanigo/reminders/RemindersActivity.java
@@ -2,6 +2,7 @@ package com.android.bbanigo.reminders;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -17,7 +18,7 @@ public class RemindersActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
 
-        mListView = (ListView)findViewById(R.id.reminders_list_view);
+        mListView = (ListView) findViewById(R.id.reminders_list_view);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
                 this, R.layout.reminders_row, R.id.row_text,
                 new String[]{"first record", "second record", "third record"});
@@ -36,13 +37,15 @@ public class RemindersActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_new:
+                Log.d(getLocalClassName(), "create new Reminder");
+                return true;
+            case R.id.action_exit:
+                finish();
+                return true;
+            default:
+                return false;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
